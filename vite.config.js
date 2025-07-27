cd4530e56059656403ecb4ef46818d7595811dc0
Chris Morrell <inxilpro@users.noreply.github.com>
2025-07-27 16:06:08 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 
+                'resources/js/app.js',
+                'resources/js/map.js',
+                'node_modules/leaflet/dist/leaflet.css',
+            ],
             refresh: true,
             detectTls: 'laracon.test',
         }),
